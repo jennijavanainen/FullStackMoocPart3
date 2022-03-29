4626fe556b87ff4defbae905a4392d82ff1c4bde
jennijavanainen <jenni.kupukupu@gmail.com>
2022-03-29 13:43:47 +0300

    Puhelinluettelo /3.6 tehty

---
+++ index.js
@@ -0,0 +1,93 @@
+const express = require('express')
+const app = express()
+
+app.use(express.json())
+
+let persons = [
+    {
+        id: 1,
+        name: "Arto Hellas",
+        number: "040-123456"
+    },
+    {
+        id: 2,
+        name: "Ada Lovelace",
+        number: "39-44-5323523"
+    },
+    {
+        id: 3,
+        name: "Dan Abramov",
+        number: "12-43-234345"
+    },
+    {
+        id: 4,
+        name: "Mary Poppendick",
+        number: "39-23-6423122"
+    },
+]
+
+const randomNumber = () => {
+    return Math.floor(Math.random()* 500)
+}
+
+app.get('/', (req, res) => {
+    res.send('<h1>Hello World</h1>')
+})
+
+app.get('/api/persons', (req, res) => {
+    res.json(persons)
+})
+
+app.post('/api/persons', (req, res) => {
+    const body = req.body
+
+    if (!body.name) {
+        return res.status(400).json({
+            error: 'name missing'
+        })
+    } else if (!body.number) {
+        return res.status(400).json({
+            error: 'number missing'
+        })
+    } else if (persons.map(person => person.name).includes(body.name)) {
+        return res.status(400).json({
+            error: 'name must be unique'
+        })
+    }
+    const person = {
+        id: randomNumber(),
+        name: body.name,
+        number: body.number
+    }
+
+    console.log(person)
+    persons = persons.concat(person)
+    res.json(person)
+
+})
+
+app.get('/api/persons/:id', (req, res) => {
+    const id = Number(req.params.id)
+    const person = persons.find(person => person.id === id)
+    if (person) {
+        res.json(person)
+    } else {
+        res.status(404).end()
+    }
+})
+
+app.get('/info', (req, res) => {
+    res.send(`<p>Phonebook has info for ${persons.length} people</p>
+                    <p>${ new Date()}</p>`)
+})
+
+app.delete('/api/persons/:id', (req, res) => {
+    const id = Number(req.params.id)
+    persons = persons.filter(note => note.id !== id)
+
+    res.status(204).end()
+})
+
+const PORT = 3001
+app.listen(PORT)
+console.log(`Server running on port ${PORT}`)
