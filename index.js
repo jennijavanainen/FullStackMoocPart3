c44b59315dee931611c5d405440b5833efbbdabc
jennijavanainen <jenni.kupukupu@gmail.com>
2022-03-31 14:56:03 +0300

    puhelinluettelo / 3.15 tehty,
    tietokannasta poistaminen

--- index.js
+++ index.js
@@ -4,53 +4,39 @@ const app = express()
 const morgan = require('morgan')
 const cors = require('cors')
 const Person = require('./models/person')
-const mongoose = require("mongoose");
 
+app.use(express.static('build'))
 app.use(express.json())
 app.use(cors())
-app.use(express.static('build'))
 
 morgan.token('body', (req) => {
     return JSON.stringify(req.body)
 })
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body'))
 
-let people = [
-    {
-        id: 1,
-        name: "Arto Hellas",
-        number: "040-123456"
-    },
-    {
-        id: 2,
-        name: "Ada Lovelace",
-        number: "39-44-5323523"
-    },
-    {
-        id: 3,
-        name: "Dan Abramov",
-        number: "12-43-234345"
-    },
-    {
-        id: 4,
-        name: "Mary Poppendick",
-        number: "39-23-6423122"
-    },
-]
+const errorHandler = (error, request, response, next) => {
+    console.error(error.message)
+
+    if (error.name === 'CastError') {
+        return response.status(400).send({ error: 'malformatted id' })
+    }
+
+    next(error)
+}
+
+app.use(errorHandler)
 
 
 const randomNumber = () => {
     return Math.floor(Math.random()* 500)
 }
 
-app.get('/', (req, res) => {
-    res.send('<h1>Hello World</h1>')
-})
+let people = []
 
 app.get('/api/persons', (req, res) => {
     Person.find({}).then(result => {
+        people = result
         res.json(result)
-        //mongoose.connection.close()
     })
 })
 
@@ -81,34 +67,38 @@ app.post('/api/persons', (req, res) => {
     })
 })
 
-app.get('/api/persons/:id', (req, res) => {
-    Person.findById(req.params.id).then(person => {
-        res.json(person)
-    }).catch ((error) => {
-        res.status(404).end()
-    })
+app.get('/api/persons/:id', (req, res, next) => {
+    Person.findById(req.params.id)
+        .then(person => {
+            if (person) {
+                res.json(person)
+            } else {
+                res.status(404).end()
+            }
+    }).catch (error => next(error))
+})
 
-    /*
-    const id = Number(req.params.id)
-    const person = people.find(person => person.id === id)
-    if (person) {
-        res.json(person)
-    } else {
-        res.status(404).end()
+app.delete('/api/persons/:id', (req, res, next) => {
+    Person.findByIdAndDelete(req.params.id)
+        .then(result => {
+            res.status(204).end()
+        })
+        .catch(error => next(error))
+})
+
+app.put('/api/persons/:id', (req, res, next) => {
+    const body = req.body
+
+    const person = {
+        id: randomNumber(),
+        name: body.name,
+        number: body.number
     }
-     */
-})
-
-app.get('/info', (req, res) => {
-    res.send(`<p>Phonebook has info for ${people.length} people</p>
-                    <p>${ new Date()}</p>`)
-})
-
-app.delete('/api/persons/:id', (req, res) => {
-    const id = Number(req.params.id)
-    people = people.filter(note => note.id !== id)
-
-    res.status(204).end()
+    Person.findByIdAndUpdate(req.params.id, person, {new: true})
+        .then(updatedPerson => {
+            res.json(updatedPerson)
+        })
+        .catch(error => next(error))
 })
 
 const PORT = process.env.PORT
